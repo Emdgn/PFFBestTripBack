1dd2607c29257f80e124df2f9f81ff6e921a8f78
sarahborkan <sarah.behaz1@gmail.com>
2023-07-06 15:43:18 +0200

    ajout crossorigin et json ignore

--- src/main/java/com/inti/controller/GuideVoyageController.java
+++ src/main/java/com/inti/controller/GuideVoyageController.java
@@ -3,6 +3,7 @@ package com.inti.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.inti.model.GuideVoyage;
 import com.inti.repository.IGuideVoyageRepository;
 
 @RestController
+@CrossOrigin
+@JsonIgnoreProperties
 public class GuideVoyageController {
 	
 	@Autowired
