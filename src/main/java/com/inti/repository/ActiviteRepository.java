079d1b692fbfbf3a22ad90728b981f35ada7fa94
Gurvaan <gurvan4444@gmail.com>
2023-07-12 09:46:34 +0200

    Fonction updateExperiences

--- src/main/java/com/inti/repository/ActiviteRepository.java
+++ src/main/java/com/inti/repository/ActiviteRepository.java
@@ -1,11 +1,16 @@
 package com.inti.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.inti.model.Activite;
 
 @Repository
 public interface ActiviteRepository extends JpaRepository<Activite, Integer> {
-
+	@Query(value = "select id from activite where id_experience=:id_experience", nativeQuery = true)
+	List<Integer> findByIdExperience(@Param("id_experience") int id_experience);
 }
